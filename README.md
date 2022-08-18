06a098197b54008063f8041bc43018da7a698f01
Alexandre Andrade <aleandrade@gmail.com>
2022-08-18 18:55:25 +1000

    feat: 🎸 Add results

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # CryptoJs Hash Performance test
+__Native Node v16 Crypto__ _versus_ __CryptoJS__
+
+The Hashing Algorithms:
+
+- MD5
+- SHA1
+- SHA256
+- SHA512
+- SHA3
+- RIPEMD160
 
 ## Running locally
 ```bash
 yarn sls invoke local -c serverless.yml \
     -f perf \
     --app serverless-lambda-crypto
+```
+
+## Deploy to AWS
+```bash
+yarn sls deploy
 ```
--- handler.js
+++ handler.js
@@ -1,18 +1,45 @@
 "use strict"
+const cryptoNode = require("crypto")
+const cryptoJs = require("crypto-js")
+const axios = require("axios")
+const { performance, PerformanceObserver } = require("perf_hooks")
+
+const jsonPayloadUrl = "https://jsonplaceholder.typicode.com/photos"
+const algorithms = ["MD5", "SHA1", "SHA256", "SHA512", "SHA3", "RIPEMD160"]
+let counter = 0
+let results = []
 
 module.exports.cryptoPerf = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  }
+  const jsonPayload = await axios.get(jsonPayloadUrl)
+  const payload = JSON.stringify(jsonPayload.data)
+  run(payload)
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+const generateHash = (algorithm, data, engine) => {
+  if (engine === "node") {
+    return cryptoNode.createHash(algorithm).update(data).digest("hex")
+  }
+  return cryptoJs[algorithm](data).toString()
+}
+
+const run = (data) => {
+  for (let engine of ["node", "crypto-js"]) {
+    for (let algorithm of algorithms) {
+      try {
+        const perfObserver = new PerformanceObserver((list) => {
+          console.log(results[counter], list.getEntries()[0].duration)
+          counter += 1
+          performance.clearMarks()
+          perfObserver.disconnect()
+        })
+        perfObserver.observe({ entryTypes: ["function"] })
+        results.push(`${engine} : ${algorithm}`)
+        const perfWrapper = performance.timerify(generateHash)
+        const hash = perfWrapper(algorithm, data, engine)
+        console.log(`${engine}:${algorithm}\t: ${hash}`)
+      } catch (_err) {
+        console.log(`${engine}:${algorithm}\t: Not supported`)
+      }
+    }
+  }
 }
